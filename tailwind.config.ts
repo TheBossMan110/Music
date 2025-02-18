674ad40dd4eeba7f4f92c11ff54262ab5e285fa5
TheBossMan110 <syedzakihaider2006@gmail.com>
2025-02-18 18:41:12 +0500

    Music

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,62 @@
 import type { Config } from "tailwindcss";
 
+// Recursive function to flatten the color palette
+function flattenColors(colors: any, prefix = '') {
+  let result: Record<string, string> = {};
+  for (const key in colors) {
+    const value = colors[key];
+    const newKey = prefix ? `${prefix}-${key}` : key;
+
+    if (typeof value === 'object') {
+      // Recursively flatten nested colors
+      result = { ...result, ...flattenColors(value, newKey) };
+    } else {
+      result[newKey] = value;
+    }
+  }
+  return result;
+}
+
+// Plugin to add each Tailwind color as a global CSS variable
+function addVariablesForColors({ addBase, theme }: any) {
+  const allColors = flattenColors(theme('colors'));
+  const newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, value]) => [`--${key}`, value])
+  );
+  addBase({
+    ":root": newVars,
+  });
+}
+
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: '0',
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: '1',
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 } satisfies Config;
